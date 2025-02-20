92de89a20ba263f2153f9a2bf383dadd7bec433c
vojtatorr <vojtatorr@gmail.com>
2025-02-20 17:18:08 +0100

    Video 8.

--- app/Presentation/Post/PostPresenter.php
+++ app/Presentation/Post/PostPresenter.php
@@ -15,9 +15,15 @@ class PostPresenter extends Presenter
 
     public function renderShow(int $postId): void
 	{
+		$post = $this->db->table('post')->get($postId);
+
+		if(!$post){
+			$this->error('Omlováme se, ale Vámi zvolený příspěvěk neexistuje!!!', 404);
+		}
 		$this->template->post = $this->db
 			->table('post')
 			->get($postId);
+		
 	}
 
 }
